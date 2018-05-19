946350a91a8fe37690cad7bca757017d5e188dc1
beth <zhangbaige@innobuddy.com>
2018-05-19 19:55:31 +0800

    docs(chore): #000 添加UI标注图

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="layout-left">
+           Menu component
+        </div>
+        <div className="layout-right">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
       </div>
     );
   }
